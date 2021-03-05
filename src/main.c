c9f1ad2731da01ca89c0dd1373cd0265381c7f32
Nicolas-Klatzer <nicolas.klatzer@gmail.com>
2021-03-05 10:39:42 +0100

    Added basic command line input options

--- src/main.c
+++ src/main.c
@@ -2,6 +2,13 @@
 // Created by Nicolas Klatzer on 04/03/2021.
 //
 
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+#include "compiler/compile.h"
+#include "compiler/compile_errors.h"
+
+
 /// Runs the compilation process on the passed files / path
 void run_compile(char* path, int argc, char *argv[])
 {
@@ -9,6 +16,26 @@ void run_compile(char* path, int argc, char *argv[])
 }
 
 /// Starting point for the para-c compiler
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
+
+    printf("Para-C Compiler - %s\n", VERSION);
+    if(argc>=2) {
+        if (argc==2 && argv[1][0] == '-') {
+            if (argv[1][1] == 'c') {
+                printf("\nCollecting files . . .");
+                if (argv[2] != NULL) {
+                    printf("not yet implemented");
+                } else {
+                    printf(ANSI_COLOR_RED "\nNo file or directory passed for compilation!");
+                }
+                return 0;
+            }
+        }
+        printf(ANSI_COLOR_RED "Unknown Command-Line Option!");
+        return 0;
+    }
+    printf("\nOptions:");
+    printf("\n  -c {file/directory}    Compiles the specified file or directory and generates"
+           "a new directory containing the compiled c-code\n");
     return 0;
 }
---
+++ src/main.h
@@ -0,0 +1,10 @@
+//
+// Created by Nicol on 05/03/2021.
+//
+
+#ifndef PARAC_MAIN_H
+#define PARAC_MAIN_H
+
+char *VERSION = "v0.1b";
+
+#endif //PARAC_MAIN_H
