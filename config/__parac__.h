0e33daa20d3eb9764830921c75529fe26f720354
Luna-Klatzer <luna.klatzer@gmail.com>
2021-05-27 22:33:28 +0200

    Added Exception Examples, updated README.md and updated the project document

--- config/__parac__.h
+++ config/__parac__.h
@@ -1,29 +1,43 @@
 // Example __parac__.h file that won't be used, but will serve as an example for a finished build
 #pragma once
 
+#include <stdbool.h>
+
 #ifndef __PARAC___H_
 #define __PARAC___H_
 
-// If the code is included in an CPP environment which Para-C supports, it will be treated as regular C-code
+/* If the code is included in an CPP environment which Para-C supports, it will be treated as regular C-code */
 #if __cplusplus
 extern "C" {
 #endif
 
-// User Project Configuration
-const char* __name__ = "";
-const char* __description__ = "";
-const char* __author__ = "";
-const char* __version__ = "";
-const char* __license__ = "";
+/* User Project Configuration */
+const char* ph_name_ = "example_parac_header";
+const char* ph_description_ = "Example for the parac base header";
+const char* ph_author_ = "Luna Klatzer";
+const char* ph_version_ = "0.1";
+const char* ph_license_ = "GPL-3.0";
 
 /*
- Compiler Configuration -
- C-Compiler Paths are included so that using 'parac finish' the compilation will be automatically finished
-*/
+ * Compiler Configuration -
+ * C-Compiler Paths are included so that using 'parac finish' the compilation will be automatically finished
+ */
 #define __PARAC_VERSION__ "Compiler-Inserted"
 
-const char* __pcompiler_path__ = "";
-const char* __ccompiler_path__ = "";
+const char* ph_pcompiler_path_ = "";
+const char* ph_ccompiler_path_ = "";
+
+/*
+ * Types definition - Compiler-Generated
+ */
+
+// ...
+
+/*
+ * Return Types - Compiler-Generated
+ */
+
+// ...
 
 #if __cplusplus
 }
--- parac-base-library/implementation-examples/exceptions/exception.c
+++ parac-base-library/implementation-examples/exceptions/exception.c
@@ -1,37 +1,25 @@
+/* Para-C imports */
+#include "__parac__.h"
+
+/* C Imports */
 #include <stdio.h>
-#include <stdbool.h>
 
-typedef struct {
-    bool is_exception;
-    const char* exception;
-    const char* traceback;
-    bool is_null;
-    int actual_value;
-} ReturnTypeInt;
-
-typedef struct {
-    bool is_exception;
-    const char* exception;
-    const char* traceback;
-    int status_code;
-} ExitStatus;
-
-typedef union {
-    ExitStatus exit_r;
-} EntryPoint;
-
+/*
+ * A generic function serving the purpose of just returning 4
+ */
 ReturnTypeInt GenericFunction(bool error)
 {
     int local_x = 4;  // local_ prefix is for the local scope
     
     ReturnTypeInt r = {
-        .is_exception = error,
-        .is_null = false,
+        .base.is_exception = error,
+        .base.is_null = false,
         .actual_value = local_x
     };
     return r;
 }
 
+/* Example Handler which will be used in the PCL (Para-C Core Library) */
 ExitStatus HandleUncatchedException(const char* exception, const char* traceback)
 {
     // do traceback stuff
@@ -47,6 +35,7 @@ ExitStatus HandleUncatchedException(const char* exception, const char* traceback
     return r;
 }
 
+/* Converts the exit status to an Entry Type Return */
 EntryPoint ExitStatusToEntryReturn(ExitStatus s)
 {
   EntryPoint r = {
@@ -55,27 +44,32 @@ EntryPoint ExitStatusToEntryReturn(ExitStatus s)
   return r;
 }
 
-// User defined main with mangling
+/* User defined main with mangling */
 EntryPoint Entry_Main()
 {
     ReturnTypeInt r_int;
 
     // Compiler will automatically notice if there is a try-except and add the handling appropriately
     r_int = GenericFunction(false);
-    if (r_int.is_exception)
-        return ExitStatusToEntryReturn(HandleUncatchedException(r_int.exception, r_int.traceback));
+    if (r_int.base.is_exception)
+        return ExitStatusToEntryReturn(HandleUncatchedException(r_int.base.exception, r_int.base.traceback));
     int local_actual_r = r_int.actual_value;  // actual name: actual_r - local_ prefix is for the local scope
 
     printf("Return of function that did not error: %i\n", local_actual_r);
 
     r_int = GenericFunction(true);
-    if (r_int.is_exception)
-        return ExitStatusToEntryReturn(HandleUncatchedException(r_int.exception, r_int.traceback));
+    if (r_int.base.is_exception)
+        return ExitStatusToEntryReturn(HandleUncatchedException(r_int.base.exception, r_int.base.traceback));
 
     ExitStatus exit_r = { .status_code = 0 };
     return ExitStatusToEntryReturn(exit_r);
 }
 
+/*
+ * Actual main method that will be the entry-point of the program.
+ * Will be defined in the PCL (Para-C Core Library) with proper implementation and functionality.
+ * This only serves as an example for how the exception system might work
+ */
 int main()
 {
   EntryPoint r = Entry_Main();
