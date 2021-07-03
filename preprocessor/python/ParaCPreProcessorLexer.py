b9f65e2a7957b1040dad06c3d8b40791d0021cdc
Luna-Klatzer <luna.klatzer@gmail.com>
2021-07-03 21:36:14 +0200

    Updated preprocessor compiled code

--- preprocessor/python/ParaCPreProcessorLexer.py
+++ preprocessor/python/ParaCPreProcessorLexer.py
@@ -1,4 +1,4 @@
-# Generated from ./paraccompiler/core/preprocessor/ParaCPreProcessor.g4 by ANTLR 4.9.2
+# Generated from ./grammar/ParaCPreProcessor.g4 by ANTLR 4.9.2
 from antlr4 import *
 from io import StringIO
 import sys
