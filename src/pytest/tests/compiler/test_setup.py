6cc83d08687e8e4eee45745d55c9404ea805571c
Luna-Klatzer <luna.klatzer@gmail.com>
2021-07-20 01:25:43 +0200

    Added pyproject.toml

--- src/pytest/tests/compiler/test_setup.py
+++ src/pytest/tests/compiler/test_setup.py
@@ -90,14 +90,14 @@ class TestCLISetup:
         "expected,input_str,line_ending", [
             ("x y z", "x y z", "\n"),
             ("x y z", "x y z", "\r\n"),
-            ("x y z", "x // something \ny z", "\n"),
-            ("x y z", "x // something \ny z", "\r\n"),
-            ("x y z", "/* xx  */x // something \ny // x x x \r\nz", "\n"),
-            ("x y z", "/* xx  */x // something \ny // x x x \r\nz", "\r"),
-            ("x y z", "x // x \r// x \n// x \r\ny z", "\n"),
-            ("x y z", "x // x \r// x \n// x \r\ny z", "\r"),
-            ("x \ny \nz", "x // x \r\n\n// x \n// x \r\ny \nz", "\n"),
-            ("x \r\ny \r\nz", "x // x \r\n\n// x \n// x \r\ny \nz", "\r\n"),
+            ("x \ny z", "x // some \ny z", "\n"),
+            ("x \r\ny z", "x // some \ny z", "\r\n"),
+            ("\nx \ny \nz", "/* xx  */x // some \ny // x x x \r\nz", "\n"),
+            ("\rx \ry \rz", "/* xx  */x // some \ny // x x x \r\nz", "\r"),
+            ("x \n\n\ny z", "x // x \r// x \n// x \r\ny z", "\n"),
+            ("x \r\r\ry z", "x // x \r// x \n// x \r\ny z", "\r"),
+            ("x \n\n\n\ny \nz", "x // x \r\n\n// x \n// x \r\ny \nz", "\n"),
+            ("x \r\r\r\ry \rz", "x // x \r\n\n// x \n// x \r\ny \nz", "\r"),
         ]
     )
     def test_remove_comments_from_str(
