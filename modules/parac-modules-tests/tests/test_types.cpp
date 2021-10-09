133d1f161b970bb535cc845f21b8d0587ffe541c
Luna-Klatzer <luna.klatzer@gmail.com>
2021-10-09 18:18:54 +0200

    Removed unnecessary link to `gtest_main` in CMakeLists.txt and fixed wrong size in test_types.cpp (tests still not properly working)

--- modules/parac-modules-tests/tests/test_types.cpp
+++ modules/parac-modules-tests/tests/test_types.cpp
@@ -8,7 +8,7 @@
 TEST(StringTypesTest, SimpleInitialisation) {
     __pbl_type_string string;
     string = __pbl_allocate_type_string(
-        10, "hello world"
+        15, "hello world"
     );
 
     EXPECT_EQ(string.byte_size, (50 + 1) * sizeof(char));
