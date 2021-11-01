6a31c82ffcbddd7576b7a819f3ef098e4e122032
Luna-Klatzer <luna.klatzer@gmail.com>
2021-11-01 23:01:29 +0100

    Removed unneeded items after moving the PBL to a new repo

--- modules/include/pbl.c
+++
@@ -1,8 +0,0 @@
-///
-/// Main file the Para-C Base Library (Static C library)
-/// Styling and naming are similar to the Google C++ style guide
-/// @see https://google.github.io/styleguide/cppguide.html
-///
-/// @author Luna-Klatzer
-
-#include "./pbl.h"
